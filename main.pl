fb987f7665935772621e82ddc192e25a086859dc
Timothy <timochui@students.cs.ubc.ca>
2023-04-07 19:20:16 -0700

    setup flow in main

---
+++ main.pl
@@ -0,0 +1,17 @@
+:- consult('api.pl').
+:- consult('nlp.pl').
+:- consult('getRestInfo.pl').
+
+% What is a restaurant in Sydney with deals?
+
+askFor(Suggestion) :- 
+    write("Welcome!!\n"),
+    write("We are here to give you restaurant suggestions \n\n"),
+    write("What restaurants would you want to find?\n"), flush_output(current_output),
+
+    read_line_to_string(user_input, St), 
+    split_string(St, " -", " ,?.!-", ListOfWords), % ignore punctuation
+    ask(ListOfWords, Suggestion).
+askFor(Suggestion) :-
+    write("No more answers\n"),
+    start.
