057553466c6db34c88cfb736902f123b1310e33f
Timothy <timochui@students.cs.ubc.ca>
2023-04-10 00:12:02 -0700

    added new categories

--- nlp.pl
+++ nlp.pl
@@ -88,32 +88,72 @@ count(L,L,[numOfRest(1)| C],C).
 noun(["restaurant" | L], L,  C, C).
 noun(["restaurants" | L], L,  [numOfRest(5)| C], C).
 noun(["place" | L], L,  C, C).
-noun(["bar" | L], L,  [queryParam("categories", "bars"), queryParam("categories", "cocktailbars"), queryParam("categories", "wine_bars")| C], C).
+noun(["shop" | L], L,  C, C).
+noun(["shops" | L], L,  [numOfRest(5)| C], C).
+noun(["store" | L], L,  C, C).
+noun(["stores" | L], L,  [numOfRest(5)| C], C).
+noun(["house" | L], L,  C, C).
+noun(["bar" | L], L,  [queryParam("categories", "bars"), queryParam("categories", "cocktailbars"), queryParam("categories", "wine_bars"), queryParm("categories", "sportsbars") | C], C).
+noun(["bars" | L], L,  [numOfRest(5), queryParam("categories", "bars"), queryParam("categories", "cocktailbars"), queryParam("categories", "wine_bars"), queryParm("categories", "sportsbars") | C], C).
 noun(["cafe" | L], L,  [queryParam("categories", "cafes") | C], C).
-noun(["pub" | L], L,  [queryParam("categories", "pubs"), queryParam("categories", "brewpubs") | C], C).
+noun(["cafes" | L], L,  [numOfRest(5), queryParam("categories", "cafes") | C], C).
+noun(["pub" | L], L,  [queryParam("categories", "pubs"), queryParam("categories", "brewpubs"), queryParam("categories", "gastropubs")| C], C).
+noun(["pubs" | L], L,  [numOfRest(5), queryParam("categories", "pubs"), queryParam("categories", "brewpubs"), queryParam("categories", "gastropubs")| C], C).
 noun(["venue" | L], L,  [queryParam("categories", "venues") | C], C).
+noun(["venues" | L], L,  [numOfRest(5), queryParam("categories", "venues") | C], C).
 noun(["smokehouse" | L], L,  [queryParam("categories", "smokehouse") | C], C).
 noun(["steakhouse" | L], L,  [queryParam("categories", "steak") | C], C).
 noun(["bistro" | L], L,  [queryParam("categories", "bistro") | C], C).
 noun(["tapas" | L], L,  [queryParam("categories", "tapasmallplates") | C], C).
+noun(["soul", "food" | L], L,  [queryParam("categories", "soulfood") | C], C).
+noun(["diners" | L], L,  [queryParam("categories", "diners") | C], C).
+noun(["hkcafe" | L], L,  [queryParam("categories", "hkcafe") | C], C).
+noun(["hk" "cafe" | L], L,  [queryParam("categories", "hkcafe") | C], C).
+noun(["HK", "cafe" | L], L,  [queryParam("categories", "hkcafe") | C], C).
+noun(["Hong" "Kong", "cafe" | L], L,  [queryParam("categories", "hkcafe") | C], C).
+noun(["lounge" | L], L,  [queryParam("categories", "lounges") | C], C).
+noun(["lounges" | L], L,  [numOfRest(5), queryParam("categories", "lounges") | C], C).
+noun(["comfort", "food" | L], L,  [queryParam("categories", "comfortfood") | C], C).
+noun(["poutinerie" | L], L,  [queryParam("categories", "poutineries") | C], C).
+noun(["poutineries" | L], L,  [numOfRest(5), queryParam("categories", "poutineries") | C], C).
 
-adj(["cheap" | L], L, C, C).
-adj(["chinese" | L], L,  [queryParam("categories", "chinese") | C], C).
-adj(["australian" | L], L,  [queryParam("categories", "australian") | C], C).
-adj(["cantonese" | L], L,  [queryParam("categories", "cantonese") | C], C).
-adj(["australian" | L], L,  [queryParam("categories", "australian") | C], C).
-adj(["thai" | L], L,  [queryParam("categories", "thai") | C], C).
-adj(["asian", "fusion"| L], L,  [queryParam("categories", "asianfusion") | C], C).
-adj(["italian" | L], L,  [queryParam("categories", "italian") | C], C).
-adj(["malaysian" | L], L,  [queryParam("categories", "malaysian") | C], C).
-adj(["singaporean" | L], L,  [queryParam("categories", "singaporean") | C], C).
-adj(["australian" | L], L,  [queryParam("categories", "modern_australian") | C], C).
-adj(["european" | L], L,  [queryParam("categories", "modern_european") | C], C).
-adj(["mexican" | L], L,  [queryParam("categories", "mexican") | C], C).
-adj(["indian" | L], L,  [queryParam("categories", "indpak") | C], C).
-adj(["vietnamese" | L], L,  [queryParam("categories", "vietnamese") | C], C).
-adj(["lebanese" | L], L,  [queryParam("categories", "lebanese") | C], C).
-adj(["spanish" | L], L,  [queryParam("categories", "spanish") | C], C).
+
+adj(["cheap" | L], L, [jsonFilter("price", "$") | C], C).
+adj(["resonably", "priced" | L], L, [jsonFilter("price", "$$") | C], C).
+adj(["affordable" | L], L, [jsonFilter("price", "$$") | C], C).
+adj(["expensive" | L], L, [jsonFilter("price", "$$$") | C], C).
+adj(["very", "expensive" | L], L, [jsonFilter("price", "$$$$") | C], C).
+adj(["poorly", "rated" | L], L, [jsonFilter("rating", 2.0) | C], C).
+adj(["moderately", "rated" | L], L, [jsonFilter("rating", 3.5) | C], C).
+adj(["highly", "rated" | L], L, [jsonFilter("rating", 5.0) | C], C).
+adj(["Chinese" | L], L,  [queryParam("categories", "chinese") | C], C).
+
+adj(["Australian" | L], L,  [queryParam("categories", "australian") | C], C).
+adj(["Cantonese" | L], L,  [queryParam("categories", "cantonese") | C], C).
+adj(["Australian" | L], L,  [queryParam("categories", "australian") | C], C).
+adj(["Thai" | L], L,  [queryParam("categories", "thai") | C], C).
+adj(["Asian", "fusion"| L], L,  [queryParam("categories", "asianfusion") | C], C).
+adj(["Italian" | L], L,  [queryParam("categories", "italian") | C], C).
+adj(["Malaysian" | L], L,  [queryParam("categories", "malaysian") | C], C).
+adj(["Singaporean" | L], L,  [queryParam("categories", "singaporean") | C], C).
+adj(["Australian" | L], L,  [queryParam("categories", "modern_australian") | C], C).
+adj(["European" | L], L,  [queryParam("categories", "modern_european") | C], C).
+adj(["Mexican" | L], L,  [queryParam("categories", "mexican") | C], C).
+adj(["Indian" | L], L,  [queryParam("categories", "indpak") | C], C).
+adj(["Vietnamese" | L], L,  [queryParam("categories", "vietnamese") | C], C).
+adj(["Lebanese" | L], L,  [queryParam("categories", "lebanese") | C], C).
+adj(["American" | L], L,  [queryParam("categories", "newamerican") | C], C).
+adj(["traditional", "American" | L], L,  [queryParam("categories", "tradamerican") | C], C).
+adj(["British" | L], L,  [queryParam("categories", "british") | C], C).
+adj(["Canadian" | L], L,  [queryParam("categories", "newcanadian") | C], C).
+adj(["Korean" | L], L,  [queryParam("categories", "korean") | C], C).
+adj(["Cambodian" | L], L,  [queryParam("categories", "cambodian") | C], C).
+adj(["Mideastern" | L], L,  [queryParam("categories", "mideastern") | C], C).
+adj(["French" | L], L,  [queryParam("categories", "french") | C], C).
+adj(["Mediterranean" | L], L,  [queryParam("categories", "mediterranean") | C], C).
+adj(["Taiwanese" | L], L,  [queryParam("categories", "taiwanese") | C], C).
+adj(["Cajun" | L], L,  [queryParam("categories", "cajun") | C], C).
+adj(["Creole" | L], L,  [queryParam("categories", "cajun") | C], C).
 
 
 adj(["breakfast" | L], L,  [queryParam("categories", "breakfast_brunch") | C], C).
@@ -122,8 +162,20 @@ adj(["coffee" | L], L,  [queryParam("categories", "coffee") | C], C).
 adj(["burger" | L], L,  [queryParam("categories", "burgers") | C], C).
 adj(["seafood" | L], L,  [queryParam("categories", "seafood") | C], C).
 adj(["pizza" | L], L,  [queryParam("categories", "pizza") | C], C).
+adj(["salad" | L], L,  [queryParam("categories", "salad") | C], C).
+adj(["bubble", "tea" | L], L,  [queryParam("categories", "bubbletea") | C], C).
+adj(["dumpling" | L], L,  [queryParam("categories", "dumplings") | C], C).
+adj(["dimsum" | L], L,  [queryParam("categories", "dimsum") | C], C).
+adj(["bbq" | L], L,  [queryParam("categories", "bbq") | C], C).
+adj(["noodles" | L], L,  [queryParam("categories", "noodles") | C], C).
+adj(["hotpot" | L], L,  [queryParam("categories", "hotpot") | C], C).
+adj(["hotpot" | L], L,  [queryParam("categories", "sandwiches") | C], C).
+adj(["hotpot" | L], L,  [queryParam("categories", "hotdogs") | C], C).
+
+
 adj(["vegan" | L], L,  [queryParam("categories", "vegan") | C], C).
 adj(["vegetarian" | L], L,  [queryParam("categories", "vegetarian") | C], C).
-
+%% noun and adj
 adj(["bistro" | L], L,  [queryParam("categories", "bistro") | C], C).
 adj(["tapas" | L], L,  [queryParam("categories", "tapasmallplates") | C], C).
+adj(["soul", "food" | L], L,  [queryParam("categories", "soulfood") | C], C).
